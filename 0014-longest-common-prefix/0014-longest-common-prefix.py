7eb79c5b781b3949dd35521ad0c66d49e2195c5a
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-11 12:21:02 +0530

    Time: 4 ms (13.76%), Space: 12.4 MB (69.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -9,11 +9,9 @@ class Solution(object):
             return ""
 
         for i in range(len(strs[0])):
-
             char = strs[0][i]
 
             for j in range(1, len(strs)):
-
                 if i >= len(strs[j]) or strs[j][i] != char:
                     return strs[0][:i]
 
