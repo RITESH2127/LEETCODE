8c57f277a08951aa426b387c1890603fcdd2f3d6
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-16 11:02:17 +0530

    Time: 611 ms (16.92%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,13 +1,5 @@
-WITH PreviousWeather AS (
-    SELECT 
-        id,
-        recordDate,
-        temperature,
-        LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp,
-        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date
-    FROM Weather
-)
-SELECT id
-FROM PreviousWeather
-WHERE temperature > prev_temp 
-  AND DATEDIFF(recordDate, prev_date) = 1;
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
