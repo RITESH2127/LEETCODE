c25bfc00bbcb3291f0d7ae0d34b75b65f672851c
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-17 11:05:52 +0530

    Time: 8 ms (77.02%), Space: 12.3 MB (95.39%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        dp[0][0] = True
+        
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+                
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(p) + 1):
+                if p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] if p[j - 2] in {s[i - 1], '.'} else False)
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] and p[j - 1] in {s[i - 1], '.'}
+                    
+        return dp[len(s)][len(p)]
