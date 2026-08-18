55b2d850a42898115477d7084bbfe1837b4b9090
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-18 18:42:59 +0530

    Time: 520 ms (78.56%), Space: 12.3 MB (94.01%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,50 +5,37 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        def kSum(nums, target, k):
-            res = []
-            
-            if not nums:
-                return res
-            
-            # Optimization: If the target is impossible to reach given the 
-            # current smallest and largest elements, terminate early.
-            average_value = target // k
-            if average_value < nums[0] or nums[-1] < average_value:
-                return res
-            
-            # Base case: When k reduces to 2, use the standard 2Sum approach
-            if k == 2:
-                return twoSum(nums, target)
-            
-            for i in range(len(nums)):
-                if i == 0 or nums[i - 1] != nums[i]:
-                    for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
-                        res.append([nums[i]] + subset)
-            
-            return res
-
-        def twoSum(nums, target):
-            res = []
-            left, right = 0, len(nums) - 1
-            
-            while left < right:
-                curr_sum = nums[left] + nums[right]
-                
-                if curr_sum == target:
-                    res.append([nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif curr_sum < target:
-                    left += 1
-                else:
-                    right -= 1
-                    
-            return res
-
         nums.sort()
-        return kSum(nums, target, 4)
+        res = []
+        n = len(nums)
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left, right = j + 1, n - 1
+                
+                while left < right:
+                    current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if current_sum == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+                        
+                    elif current_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return res
