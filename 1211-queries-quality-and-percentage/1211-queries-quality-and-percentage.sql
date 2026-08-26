bd620d6f2de3f9556bbc093f52dc9d1a8593e33d
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-26 12:56:56 +0530

    Time: 417 ms (38.32%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,7 +2,7 @@
 SELECT 
     query_name, 
     ROUND(AVG(rating / position), 2) AS quality,
-    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage
+    ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
 FROM 
     Queries
 WHERE 
