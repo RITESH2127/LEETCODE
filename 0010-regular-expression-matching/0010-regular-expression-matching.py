e7583ac40c5c4fcb41d0f6aac2253d62304de3be
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-27 21:19:16 +0530

    Time: 299 ms (5.18%), Space: 12.2 MB (95.35%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -5,22 +5,5 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        m, n = len(s), len(p)
-        prev = [False] * (n + 1)
-        curr = [False] * (n + 1)
-        prev[0] = True
-        
-        for j in range(1, n + 1):
-            if p[j - 1] == '*':
-                prev[j] = prev[j - 2]
-                
-        for i in range(1, m + 1):
-            curr[0] = False
-            for j in range(1, n + 1):
-                if p[j - 1] == '*':
-                    curr[j] = curr[j - 2] or (prev[j] and p[j - 2] in {s[i - 1], '.'})
-                else:
-                    curr[j] = prev[j - 1] and p[j - 1] in {s[i - 1], '.'}
-            prev = curr[:]
-            
-        return prev[n]
+        import re
+        return re.match('^' + p + '$', s) is not None
