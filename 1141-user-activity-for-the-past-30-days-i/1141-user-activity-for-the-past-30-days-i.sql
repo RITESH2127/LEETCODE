2cf2934ad48e5b69980d7880ba524441b2140113
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-08-31 16:46:15 +0530

    Time: 682 ms (13.2%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -2,5 +2,6 @@
 SELECT activity_date AS day,
        COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+                        AND '2019-07-27'
 GROUP BY activity_date;
