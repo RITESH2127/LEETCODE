b977a4e0ebfaf9d60f5cb8c545bde27127f5c21b
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-09-06 13:55:51 +0530

    Time: 541 ms (92.57%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,16 +1,9 @@
 # Write your MySQL query statement below
-WITH UniquePurchases AS (
-    SELECT DISTINCT 
-        customer_id, 
-        product_key
-    FROM 
-        Customer
-)
 SELECT 
     customer_id
 FROM 
-    UniquePurchases
+    Customer
 GROUP BY 
     customer_id
 HAVING 
-    COUNT(*) = (SELECT COUNT(*) FROM Product);
+    COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
