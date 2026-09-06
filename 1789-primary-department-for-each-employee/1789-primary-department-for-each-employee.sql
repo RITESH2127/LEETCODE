586522ca11292fd672d422935a9c94d02c1198d9
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-09-06 13:54:14 +0530

    Time: 760 ms (16.2%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -2,13 +2,15 @@
 SELECT 
     employee_id, 
     department_id
-FROM 
-    Employee
+FROM (
+    SELECT 
+        employee_id,
+        department_id,
+        primary_flag,
+        COUNT(department_id) OVER(PARTITION BY employee_id) AS dept_count
+    FROM 
+        Employee
+) sub
 WHERE 
-    primary_flag = 'Y'
-    OR employee_id IN (
-        SELECT employee_id
-        FROM Employee
-        GROUP BY employee_id
-        HAVING COUNT(department_id) = 1
-    );
+    primary_flag = 'Y' 
+    OR dept_count = 1;
