0a87250e2669cafd769b541a6a2e087e7a1db1f3
Ritesh Kumar <riteshkumarnew369@gmail.com>
2026-09-07 12:09:57 +0530

    Time: 314 ms (58.53%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
 SELECT x, y, z,
-       IF(x + y > z AND x + z > y AND y + z > x, 'Yes', 'No') AS triangle
+       CASE
+           WHEN x + y > z
+            AND x + z > y
+            AND y + z > x
+           THEN 'Yes'
+           ELSE 'No'
+       END AS triangle
 FROM Triangle;
